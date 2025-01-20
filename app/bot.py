e1099a4d2d2987043ff46b69fcf85337c257e541
NicolasYapobi <nicolas.yapobi@epitech.eu>
2025-01-20 17:10:40 +0100

    feat: add bot event

--- app/bot.py
+++ app/bot.py
@@ -1,31 +1,36 @@
 import os
 import discord
 from dotenv import load_dotenv
+from discord.ext import commands
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 GUILD = os.getenv('DISCORD_GUILD')
-print(GUILD)
 
 intents = discord.Intents.default()
 intents.message_content = True
-client = discord.Client(intents=intents)
 
-@client.event
+bot = commands.Bot(intents=intents, command_prefix='!')
+
+@bot.event
 async def on_ready():
-    guild = discord.utils.get(client.guilds, name=GUILD)
+    guild = discord.utils.get(bot.guilds, name=GUILD)
     print(
-        f'{client.user} is connected to the following serv:\n'
+        f'{bot.user} is connected to the following serv:\n'
         f'{guild.name}(id: {guild.id})\n'
     )
-@client.event
-async def on_message(message):
-    print(message)
-    if message.author == client.user:
-        return
-    if message.content == "Hey":
-        response = "Oh ! Got in touch"
-        await message.channel.send(response)
+@bot.command(name="list", help="Give a list of links")
+async def on_message(ctx):
+    response = "List of links:"
+    await ctx.send(response)
     
+@bot.event
+async def on_error(event, *args, **kwargs):
+    with open('err.log', 'a') as f:
+        if event == 'on_message':
+            f.write(f'Unhandled message:: {args[0]}\n')
+        else: 
+            raise
 
-client.run(TOKEN)
+
+bot.run(TOKEN)
