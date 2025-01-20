3f5607ad8aea56a3b265c28c27dd37280ee9c5e4
NicolasYapobi <nicolas.yapobi@epitech.eu>
2025-01-20 16:43:39 +0100

    feat: add message event handler

--- app/bot.py
+++ app/bot.py
@@ -4,11 +4,28 @@ from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
+GUILD = os.getenv('DISCORD_GUILD')
+print(GUILD)
 
-client = discord.Client(intents=discord.Intents.default())
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    guild = discord.utils.get(client.guilds, name=GUILD)
+    print(
+        f'{client.user} is connected to the following serv:\n'
+        f'{guild.name}(id: {guild.id})\n'
+    )
+@client.event
+async def on_message(message):
+    print(message)
+    if message.author == client.user:
+        return
+    if message.content == "Hey":
+        response = "Oh ! Got in touch"
+        await message.channel.send(response)
+    
 
 client.run(TOKEN)
